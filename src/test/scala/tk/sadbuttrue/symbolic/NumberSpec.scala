6be789d523e2cefa47f3178dbcbf9a140423725c
Eugene Aslanov <aslanov.ev@gmail.com>
2016-10-17 01:03:57 +0400

    Add multiply test for numbers

--- src/test/scala/tk/sadbuttrue/symbolic/NumberSpec.scala
+++ src/test/scala/tk/sadbuttrue/symbolic/NumberSpec.scala
@@ -22,6 +22,29 @@ class NumberSpec extends WordSpec with Matchers {
         eval(n1 - n2) shouldEqual 8d
       }
     }
+    "multiply" should {
+      "be 0 if multiply on 0" in {
+        val n1 = Number(0)
+        val n2 = Number(9)
+        eval(n1 * n2) shouldEqual 0d
+        eval(n2 * n1) shouldEqual 0d
+      }
+      "be expression if multiply on 1" in {
+        val n1 = Number(1)
+        val n2 = Number(9)
+        val v1 = Variable("x")
+        eval(n1 * n2) shouldEqual 9d
+        eval(n2 * n1) shouldEqual 9d
+        n1 * v1 shouldEqual v1
+        v1 * n1 shouldEqual v1
+      }
+      "2 * 9 = 18" in {
+        val n1 = Number(2)
+        val n2 = Number(9)
+        eval(n1 * n2) shouldEqual 18d
+        n1 * n2 shouldEqual Number(18)
+      }
+    }
     "derive" should {
       "return 0 with any variable" in {
         eval(Number(10) derive Variable("x")) shouldEqual 0d
