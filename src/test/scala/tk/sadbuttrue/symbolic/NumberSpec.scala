5c314e372e54ec9244293fb5e036333ed0faab1e
Eugene Aslanov <aslanov.ev@gmail.com>
2016-10-17 00:55:58 +0400

    Add sub test for numbers

--- src/test/scala/tk/sadbuttrue/symbolic/NumberSpec.scala
+++ src/test/scala/tk/sadbuttrue/symbolic/NumberSpec.scala
@@ -15,6 +15,13 @@ class NumberSpec extends WordSpec with Matchers {
         eval(n1 + n2) shouldEqual 10d
       }
     }
+    "sub" should {
+      "9 - 1 = 8" in {
+        val n1 = Number(9)
+        val n2 = Number(1)
+        eval(n1 - n2) shouldEqual 8d
+      }
+    }
     "derive" should {
       "return 0 with any variable" in {
         eval(Number(10) derive Variable("x")) shouldEqual 0d
